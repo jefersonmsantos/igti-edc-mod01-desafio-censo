37035459f6c9b9e8ba3bb573b76f85c31db2b418
Jeferson Machado Santos <jefersonmsantos@outlook.com>
2021-08-12 15:38:51 -0300

    bootstrap - spark adjust

--- etl/csv_to_parquet_job_spark.py
+++ etl/csv_to_parquet_job_spark.py
@@ -1,37 +1,53 @@
 from pyspark.sql.functions import mean, max, min, col,count
 from pyspark.sql import SparkSession
-import boto3
+import s3fs
+#import boto3
 
-spark = (SparkSession.builder.appName("DeltaExercise")
-    .config("spark.jars.packages", "io.delta:delta-core_2.12:1.0.0")
-    .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension")
-    .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")
+#spark = (SparkSession.builder.appName("DeltaExercise")
+#    .config("spark.jars.packages", "io.delta:delta-core_2.12:1.0.0")
+#    .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension")
+#    .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")
+#    .getOrCreate()
+#)
+
+spark = (
+    SparkSession.builder.appName("ExerciseSpark")
     .getOrCreate()
 )
 
-s3 = boto3.resource('s3')
-bucket = s3.Bucket('datalake-igti-mod01-desafio/raw-data')
+#from delta.tables import *
+#s3 = boto3.resource('s3')
+#bucket = s3.Bucket('datalake-igti-mod01-desafio')
 
-for obj in bucket.objects.all():
+#for obj in bucket.objects.all():
         
-    if str(obj.key)[-3:].lower() == "csv":
+#    if str(obj.key)[-3:].lower() == "csv":
         
+        #file_name = obj.key.split("/")[-1]
+
+fs = s3fs.S3FileSystem(anon=False)
+
+for obj in fs.ls('datalake-igti-mod01-desafio'):
+    if obj[-3:].lower() == 'csv':
+
         raw_file = (
-                    spark
-                    .read
-                    .format("csv")
-                    .option("header", True)
-                    .option("inferSchema",True)
-                    .option("delimiter",";")
-                    .option("encoding","utf-8")
-                    .load('s3://datalake-igti-mod01-desafio/raw-data/'+obj.key)
-                )
+                spark
+                .read
+                .format("csv")
+                .option("header", True)
+                .option("inferSchema",True)
+                .option("delimiter","|")
+                .option("encoding","utf-8")
+                .load('s3://datalake-igti-mod01-desafio/'+obj)
+            )
+
+        file_name = obj.split("/")[-1][:-4]
 
         #salvar parquet
         (
             raw_file
             .write
             .mode("overwrite")
-            .format("delta")
-            .save('s3://datalake-igti-mod01-desafio/staging/'+obj.key[:-4])
+            .format("parquet")
+            .save('s3://datalake-igti-mod01-desafio/staging/'+file_name)
         )
--- infrastructure/s3_files.tf
+++ infrastructure/s3_files.tf
@@ -5,3 +5,11 @@ resource "aws_s3_bucket_object" "codigo_spark" {
     source = "../etl/csv_to_parquet_job_spark.py"
     etag = filemd5("../etl/csv_to_parquet_job_spark.py")
 }
+
+resource "aws_s3_bucket_object" "codigo_spark" {
+    bucket = aws_s3_bucket.datalake.id
+    key = "emr-code/pyspark/pip_install.sh"
+    acl = "private"
+    source = "../etl/pip_install.sh"
+    etag = filemd5("../etl/pip_install.sh")
+}
